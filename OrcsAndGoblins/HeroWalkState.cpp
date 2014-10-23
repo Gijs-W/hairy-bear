09120149f9c1a84623ea513f3677fe0c6f2ddef0
Gijs-W <gijsvandewater+github@gmail.com>
2014-10-23 15:50:51 +0200

    CurrentLevel / Element uit lijst halen

--- OrcsAndGoblins/HeroWalkState.cpp
+++ OrcsAndGoblins/HeroWalkState.cpp
@@ -14,23 +14,34 @@ void HeroWalkState::handle(Engine* context) {
 
 	if (direction == "noord") {
 		hero->setPosY(hero->getPosY() + 1);
-		printf("Hero gaat naar noord");
+		printf("Hero gaat naar noord\n");
 	}
 	else if (direction == "zuid") {
 		hero->setPosY(hero->getPosY() - 1);
-		printf("Hero gaat naar zuid");
+		printf("Hero gaat naar zuid\n");
 	}
 	else if (direction == "oost") {
 		hero->setPosX(hero->getPosX() + 1);
-		printf("Hero gaat naar oost");
+		printf("Hero gaat naar oost\n");
 	}
 	else if (direction == "west") {
 		hero->setPosX(hero->getPosX() - 1);
 		printf("Hero gaat naar west");
 	}
+	handleHeroPosition(hero, context);
 
 	// als we in combat komen switchen naar combatstate ipv MapDrawState
 	context->setState(new MapDrawState());
 
 	delete expectedAnswers;
+}
+
+void HeroWalkState::handleHeroPosition(Hero* hero, Engine* context ) {
+	MapType* maptype = context->getMaps()->at(Map::currentLevel).getMapType(hero->getPosX(), hero->getPosX());
+	
+	switch (maptype->getType()) {
+	case Tile::Room:
+		break;
+	}
+
 }
