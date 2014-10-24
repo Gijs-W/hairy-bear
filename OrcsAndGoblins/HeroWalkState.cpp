c0d15a24f3df21967e5f8f2fcb2542159be4b684
Gijs-W <gijsvandewater+github@gmail.com>
2014-10-24 11:32:25 +0200

    HeroWalk validation

--- OrcsAndGoblins/HeroWalkState.cpp
+++ OrcsAndGoblins/HeroWalkState.cpp
@@ -2,13 +2,20 @@
 #include "HeroWalkState.h"
 #include "RoomViewState.h"
 void HeroWalkState::handle(Engine* context) {
-	vector<string> *expectedAnswers = new vector < string > ;
-	expectedAnswers->push_back("noord");
-	expectedAnswers->push_back("zuid");
-	expectedAnswers->push_back("oost");
-	expectedAnswers->push_back("west");
-
 	Hero* hero = context->getHero();
+	MapType* northMap = context->getMaps()->at(Map::currentLevel).getMapType(hero->getPosX(), hero->getPosY() -1);
+	MapType* southMap = context->getMaps()->at(Map::currentLevel).getMapType(hero->getPosX(), hero->getPosY() + 1);
+	MapType* eastMap = context->getMaps()->at(Map::currentLevel).getMapType(hero->getPosX()+1, hero->getPosY());
+	MapType* westMap = context->getMaps()->at(Map::currentLevel).getMapType(hero->getPosX()-1, hero->getPosY());
+
+
+	vector<string> *expectedAnswers = new vector < string > ;
+	if (isWalkableTile(northMap)) expectedAnswers->push_back("noord");
+	if (isWalkableTile(southMap)) expectedAnswers->push_back("zuid");
+	if (isWalkableTile(eastMap)) expectedAnswers->push_back("oost");
+	if (isWalkableTile(westMap)) expectedAnswers->push_back("west");
+
+	
 
 	string direction = InputManager::getInstance()->requestInput("Welke Richting?", expectedAnswers);
 
@@ -36,7 +43,7 @@ void HeroWalkState::handle(Engine* context) {
 }
 
 void HeroWalkState::handleHeroPosition(Hero* hero, Engine* context ) {
-	MapType* maptype = context->getMaps()->at(Map::currentLevel).getMapType(hero->getPosX(), hero->getPosX());
+	MapType* maptype = context->getMaps()->at(Map::currentLevel).getMapType(hero->getPosX(), hero->getPosY());
 	
 	// ooit nog is een visitor pattern hier inbouwen
 	Room* room = dynamic_cast<Room*>(maptype);
@@ -47,5 +54,20 @@ void HeroWalkState::handleHeroPosition(Hero* hero, Engine* context ) {
 	else {
 		//context->setState(new RoomViewState(new Room()));
 		context->setState(new MapDrawState());
+	}
+}
+
+bool HeroWalkState::isWalkableTile(MapType* type) {
+	if (type == nullptr) return false;
+
+	Tile tile = type->getType();
+
+	switch (tile) {
+		/* falltrough*/
+	case Tile::CorridorVertical:
+	case Tile::CorridorHorizontal:
+		return false;
+	default:
+		return true;
 	}
 }
